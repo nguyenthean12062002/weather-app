e217001a173b80b07f22f973224b29e7b12fe74e
Nguyễn Thế An <110209220+nguyenthean12062002@users.noreply.github.com>
2023-04-01 20:09:27 +0700

    load code

---
+++ app.js
@@ -0,0 +1,59 @@
+let input = document.querySelector(".before_input")
+let address =document.querySelector(".header__name")
+let desWe = document.querySelector(".main__des")
+let showDay = document.querySelector(".main__day-show")
+let showTemperature = document.querySelector(".main__temperature")
+let showWind = document.querySelector(".speed-wind--show")
+let api;
+let apiKey = "5609b3378c50f5eef2bd4474f87f2b3f";
+input.addEventListener("keyup", (e) =>{
+    if(e.key =="Enter" && input.value != ""){
+        requestApi(input.value);
+    }
+})
+function requestApi(name){
+    api = `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${apiKey}`
+    fechData();
+}
+function fechData(){    
+   fetch(api)
+    .then(
+        (response) =>{
+            return response.json();
+        }    
+    )
+    .then((data) =>{
+        if(data.name === undefined){
+            address.innerHTML = "Vị Trí Không Tồn Tại"
+            input.classList.add("erorr")
+        }
+        else{
+            input.classList.remove("erorr")
+            address.innerHTML =data.name;
+        }
+        let temperatura =Object.entries( data.main)
+        temperatura.forEach(([key,value]) =>{
+            if(key == "temp"){
+                showTemperature.innerHTML = value;
+            }
+        })
+        let desWeather = data.weather.forEach((info) =>{
+            desWe.innerHTML = info.description;
+        })
+        let winds = Object.entries(data.wind)
+        winds.forEach(([wind, windValue]) =>{
+            if(wind == "speed"){
+                showWind.innerHTML = windValue;
+            }
+        })
+        function getDay (){
+            let today = new Date();
+            let day = today.getDate();
+            let month = today.getMonth();
+            showDay.innerHTML = `
+            ${day}/${month}
+            `
+        }
+        getDay();
+    })
+}
